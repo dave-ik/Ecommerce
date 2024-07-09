d1afe767e0bb7378208bb56052d7625a8f408f13
dave-ik <david.ikwuegbu@pau.edu.ng>
2024-07-09 14:10:35 +0100

    Initial Commit

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,134 @@
+import React from 'react';
+import { FaFacebook, FaInstagram, FaLinkedin, FaLocationArrow, FaMobileAlt } from "react-icons/fa";
+
+const FooterLinks = [
+  {
+    title: "House",
+    link: "/#",
+  },
+  {
+    title: "About",
+    link: "/#about",
+  },
+  {
+    title: "Contact",
+    link: "/#contact",
+  },
+  {
+    title: "Blog",
+    link: "/#blog",
+  },
+]
+
+const Footer = () => {
+  return (
+    <div className='dark:bg-gray-950'>
+      <div className="container">
+        <div className="grid md:grid-cols-3 pb-20 pt-5">
+          {/* <company details> */}
+          <div className='py-8 px-4'>
+            <a
+              href="#"
+              className="text-primary font-bold tracking-wide test-2xl togglecase sm:text-3xl"
+
+            >
+              PAUmart
+            </a>
+            <p><br></br></p>
+            <p>
+              <h3 class="widgettitle">Contact Us</h3>
+            </p>
+            <p className='text-gray-600 dark:text-white/70 lg:pr-24 pt-3'>
+              Lines are open:
+              <br></br>
+              Monday - Friday
+              <br></br>
+              8.30am - 5.00pm
+            </p>
+            <a
+              href="https://pau.edu.ng/"
+              target='_blank'
+              className='inline-block bg-primary/90 text-white py-2 px-4 mt-4 text-sm rounded-full ml-[-17px]'>
+              Official School Website
+            </a>
+          </div>
+
+          {/* Footer links */}
+          <div className='col-span-2 grid grid-cols-2 sm:grid-cols-3 md:pl-10'>
+            <div className='py-8 px-4'>
+              <h1 className='text-xl font-bold sm:text-left mb-3'>Important Links</h1>
+              <ul className='space-y-3'>
+                {FooterLinks.map(
+                  (data, index) => (
+                    <li key={index}>
+                      <a
+                        href={data.link}
+                        className='text-gray-600 dark:text-gray-400 hover:dark:text-white hover:text-black duration-300'
+                      >
+                        {data.title}
+                      </a>
+                    </li>
+                  )
+                )
+                }
+              </ul>
+            </div>
+            {/* second col links */}
+            <div className='py-8 px-4'>
+              <h1 className='text-xl font-bold sm:text-left mb-3'>Quick Links</h1>
+              <ul className='space-y-3'>
+                {FooterLinks.map(
+                  (data, index) => (
+                    <li key={index}>
+                      <a
+                        href={data.link}
+                        className='text-gray-600 dark:text-gray-400 hover:dark:text-white hover:text-black duration-300'
+                      >
+                        {data.title}
+                      </a>
+                    </li>
+                  )
+                )
+                }
+              </ul>
+            </div>
+
+            {/* Company Address */}
+            <div className='py-8 px-4 col-span-2 sm:col-auto'>
+              <h1 className='text-xl font-bold sm:text-left mb-3'>Reach Out</h1>
+              <div>
+                <div className="flex items-center gap-3">
+                  <FaLocationArrow />
+                  <p>Km 52, Ibeju-Lekki, PO Box 73688, Lagos</p>
+                </div>
+                <div className='flex items-center gap-3 mt-6'>
+                  <FaMobileAlt />
+                  <p>+234 816 482 0228</p>
+                </div>
+                <div className='flex items-center gap-3'>
+                  <FaMobileAlt />
+                  <p>+234 708 864 1465</p>
+                </div>
+
+                {/* social links */}
+                <div className='flex items-center gap-3 mt-6'>
+                  <a href="#">
+                    <FaInstagram className='text-3xl hover:text-primary duration-300' />
+                  </a>
+                  <a href="#">
+                    <FaFacebook className='text-3xl hover:text-primary duration-300' />
+                  </a>
+                  <a href="#">
+                    <FaLinkedin className='text-3xl hover:text-primary duration-300' />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
