37e96391964e171563e3141e07465477c5948da9
dave-ik <david.ikwuegbu@pau.edu.ng>
2024-07-15 19:36:00 +0100

    Almost Done

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -94,7 +94,7 @@ const Navbar = ({ handleOrderPopup }) => {
                         <button className='relative p-3' onClick={handleCartClick}>
                             <FaCartShopping className='text-xl text-gray-600 dark:text-gray-400' />
                             <div className='w-4 h-4 bg-red-500 text-white rounded-full absolute top-0 right-0 flex items-center justify-center text-xs'>
-                                4
+                                
                             </div>
                         </button>
                         {/* Dark Mode section */}
